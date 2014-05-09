ed658dc2893fbf8c04da48e05f965eab28a7feb5
Oleg Levy <oleg.levy@live.com>
2014-05-09 17:21:12 +0900

    need to sync things up

---
+++ tests/testData.js
@@ -0,0 +1,53 @@
+/*
+
+ http://jsoneditoronline.org/
+*/
+
+
+var jsonData = {
+    "kanjiTranslations":[{
+        "kebs": [
+            {
+                "k":"紅葉",
+                "inf": [],
+                "pri": ["ichi1", "news1", "nf16"]
+            }
+        ],
+        "rebSenses": [{
+            "readings": [{
+                "r": "こうよう",
+                "nokanji": false,
+                "inf": [],
+                "pri": ["ichi1", "news1", "nf16"]
+            }, {
+                "r": "もみじ",
+                "nokanji": false,
+                "inf": [],
+                "pri": ["ichi1"]
+            }],
+            "senses": [{
+                "pos": ["n", "vs"],
+                "glosses": ["autumn colours", "fall colors", "leaves changing color (colour)"]
+            }]
+        }, {
+            "readings": [{
+                "r": "こうよう",
+                "nokanji": false,
+                "pri": ["ichi1", "news1", "nf16"]
+            }],
+            "senses": [{
+                "glosses": ["leaves turning red", "red leaves"]
+            }]
+        }, {
+            "readings": [{
+                "r": "もみじ",
+                "nokanji": false,
+                "pri": ["ichi1"]
+            }],
+            "senses": [{
+                "pos": "n",
+                "glosses": ["(Japanese) maple (Acer japonicum)"]
+            }]
+        }]
+    }]
+};
